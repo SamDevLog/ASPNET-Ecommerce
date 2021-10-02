33e65f56d85ee4a4b766b863980216bdf5db0ba3
Samir <65616743+Chapali9a@users.noreply.github.com>
2021-10-02 20:31:29 +0100

    Added Generic Repository, Specification pattern, and Mapping api returned objects

---
+++ Core/Specifications/BaseSpecification.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Core.Specifications {
+    public class BaseSpecification<T> : ISpecification<T> {
+        public BaseSpecification () { }
+
+        public BaseSpecification (Expression<Func<T, bool>> criteria) {
+            Criteria = criteria;
+        }
+
+        public Expression<Func<T, bool>> Criteria { get; }
+
+        public List<Expression<Func<T, object>>> Includes { get; } = new ();
+
+        protected void AddInclude (Expression<Func<T, Object>> includeExpression) {
+            Includes.Add (includeExpression);
+        }
+    }
+}
