3e62acd6c764ed8705040321c5ac3737db745565
Samir <65616743+Chapali9a@users.noreply.github.com>
2021-10-12 01:23:58 +0100

    Added redis support for saving the basket in the backend and docker container for redis

--- API/Startup.cs
+++ API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
 
 namespace API {
     public class Startup {
@@ -23,6 +24,11 @@ namespace API {
             services.AddAutoMapper (typeof (MappingProfiles));
 
             services.AddDbContext<StoreContext> (x => x.UseSqlite (_config.GetConnectionString ("DefaultConnection")));
+
+            services.AddSingleton<IConnectionMultiplexer> (c => {
+                var configuration = ConfigurationOptions.Parse (_config.GetConnectionString ("Redis"), true);
+                return ConnectionMultiplexer.Connect (configuration);
+            });
 
             services.AddApplicationServices ();
 
